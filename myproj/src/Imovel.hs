e1c49adecbf990aa085bce18dc886721780345cc
Haemax <haendelandrade@gmail.com>
2022-11-29 09:02:45 -0300

    imovel gets and sets

--- myproj/src/Imovel.hs
+++ myproj/src/Imovel.hs
@@ -10,3 +10,30 @@ module Imovel (Imovel(..), getEndereco) where
     getEndereco :: Imovel -> String
     getEndereco Imovel {endereco = e} = e
 
+    setEndereco :: String -> Imovel -> Imovel
+    setEndereco end im = im {endereco = end}
+
+    getCidade :: Imovel -> String
+    getCidade Imovel {cidade = e} = e
+
+    setCidade :: String -> Imovel -> Imovel
+    setCidade cid im = im {cidade = cid}
+
+    getBairro :: Imovel -> String
+    getBairro im = im {bairro = bar}
+    
+    setBairro :: String -> Imovel -> Imovel
+    setBairro bar im = im {bairro = bar}
+    
+    getPreco :: Imovel -> String
+    getPreco Imovel {preco = e} = e
+
+    setPreco :: String -> Imovel -> Imovel
+    setPreco prec im = im {preco = prec}
+
+    isReservado :: Imovel -> String
+    isReservado Imovel {estaReservado = e} = e
+
+    setReservado :: Bool -> Imovel -> Imovel
+    setEndereco x im = im {isReservado = x}
+
