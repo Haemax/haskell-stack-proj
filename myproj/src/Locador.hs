cc5777b6726a80583c67e8abea9e825f1906b7bd
LuizAntonio67 <luiz.cardozo@ccc.ufcg.edu.br>
2022-11-29 17:54:43 -0300

    Escrita de arquivos feita

--- myproj/src/Locador.hs
+++ myproj/src/Locador.hs
@@ -1,14 +1,20 @@
 module Locador (
-    Apartamento(..),
+    Imovel(..),
     Endereco(..),
     Agenda(..),
     cadastrarImovel,
+    salvarImovel,
+    salvarEndereco,
     criarReserva
 ) where
 
--- Tipo do apartamento, com os atributos: Valor da diaria (Double), Numero de telefone para contato (String), Tipo de imóvel (String), Quantidade de quartos (Integer), Quantidade de quartos com suíte (Integer), Quantidade de banheiros (Integer), Número de vagas de garagem (Integer), Limite de hóspedes (Integer), Se tem wifi (Bool), Se tem piscina (Bool), Se aceita animais (Bool), Endereço (Endereco).
-data Apartamento = Apartamento
-  { valorDiaria :: Double,
+-- Tipo do imovel, com os atributos: Valor da diaria (Double), Numero de telefone para contato (String), 
+-- Tipo de imóvel (String), Quantidade de quartos (Integer), Quantidade de quartos com suíte (Integer), 
+-- Quantidade de banheiros (Integer), Número de vagas de garagem (Integer), Limite de hóspedes (Integer), 
+-- Se tem wifi (Bool), Se tem piscina (Bool), Se aceita animais (Bool), Endereço (Endereco).
+data Imovel = Imovel
+  { idImovel :: Integer,
+    valorDiaria :: Double,
     telefoneContato :: String,
     tipoImovel :: String,
     quantQuartos :: Integer,
@@ -47,10 +53,14 @@ data Agenda = Agenda
 -- Coloca um apartamento como disponível para alugar
 cadastrarImovel :: IO ()
 cadastrarImovel = do
+  putStrLn "ID do imóvel: "
+  idImovel <- readLn :: IO Integer
   putStrLn "Valor da diária: "
   diaria <- readLn :: IO Double
   putStrLn "Telefone para contato: "
   telefone <- getLine
+  putStrLn "CPF: "
+  cpf <- getLine
   putStrLn "Tipo do imóvel: "
   tipo <- getLine
   putStrLn "Quantidade de quartos: "
@@ -70,36 +80,72 @@ cadastrarImovel = do
   putStrLn "Aceita animais? "
   animais <- getLine
 
-  putStrLn "CEP: "
-  cep <- getLine
-  putStrLn "Rua: "
-  rua <- getLine
-  putStrLn "Número: "
-  numero <- getLine
-  putStrLn "Complemento: "
-  complemento <- getLine
-  putStrLn "Bairro: "
-  bairro <- getLine
-  putStrLn "Cidade: "
-  cidade <- getLine
-  putStrLn "Estado: "
-  estado <- getLine
-
-  let endereco = Endereco cep rua numero complemento bairro cidade estado
-  let apto = Apartamento diaria telefone tipo quartos quartosSuite banheiros garagens hospedes wifi piscina animais endereco
+  salvarImovel idImovel diaria telefone cpf tipo quartos quartosSuite banheiros garagens hospedes wifi piscina animais
+  cadastrarEndereco idImovel
   putStrLn "Imóvel cadastrado."
+
+cadastrarEndereco :: Integer -> IO ()
+cadastrarEndereco idImovel = do
+    putStrLn "CEP: "
+    cep <- getLine
+    putStrLn "Rua: "
+    rua <- getLine
+    putStrLn "Número: "
+    numero <- getLine
+    putStrLn "Complemento: "
+    complemento <- getLine
+    putStrLn "Bairro: "
+    bairro <- getLine
+    putStrLn "Cidade: "
+    cidade <- getLine
+    putStrLn "Estado: "
+    estado <- getLine
+
+    salvarEndereco idImovel cep rua numero complemento bairro cidade estado
 
 criarReserva :: IO ()
 criarReserva = do
-  putStrLn "Id do apartamento a ser reservado: "
-  idApto <- getLine
+  putStrLn "Id do imóvel a ser reservado: "
+  idImovel <- readLn :: IO Integer
   putStrLn "Dia da reserva: "
   dia <- readLn :: IO Integer
   putStrLn "Mês da reserva: "
   mes <- readLn :: IO Integer
 
-  let reserva = Agenda idApto dia mes True
+  salvarReserva idImovel dia mes
   putStrLn "Reserva feita."
+
+-- Tipo do imovel, com os atributos: ID do imóvel (Integer), Valor da diaria (Double), Número de telefone para contato (String), 
+-- Tipo de imóvel (String), Quantidade de quartos (Integer), Quantidade de quartos com suíte (Integer), 
+-- Quantidade de banheiros (Integer), Número de vagas de garagem (Integer), Limite de hóspedes (Integer), 
+-- Se tem wifi (String), Se tem piscina (String), Se aceita animais (String).
+salvarImovel :: Integer -> Double -> String -> String -> String -> Integer -> Integer -> Integer -> Integer -> Integer -> String -> String -> String -> IO ()
+salvarImovel idImovel diaria telefone cpf tipo quartos quartosSuite banheiros garagens hospedes wifi piscina animais = do
+    let imovel = show idImovel ++ "," ++ show diaria ++ "," ++ telefone ++ "," ++ cpf ++ "," ++ tipo ++ "," ++ show quartos ++ "," ++ show quartosSuite ++ "," ++ show banheiros ++ "," ++ show garagens ++ "," ++ show hospedes ++ "," ++ wifi ++ "," ++ piscina ++ "," ++ animais ++ "\n" 
+    appendFile "Imoveis.txt" imovel
+
+salvarEndereco :: Integer -> String -> String -> String -> String -> String -> String -> String -> IO ()
+salvarEndereco idImovel cep rua numero complemento bairro cidade estado = do
+    let endereco = show idImovel ++ "," ++ cep ++ "," ++ rua ++ "," ++ numero ++ "," ++ complemento ++ "," ++ bairro ++ "," ++ cidade ++ "," ++ estado ++ "\n"
+    appendFile "Enderecos.txt" endereco
+
+salvarReserva :: Integer -> Integer -> Integer -> IO ()
+salvarReserva idImovel dia mes = do
+    let reserva = show idImovel ++ "," ++ show dia ++ "," ++ show mes ++ "\n"
+    appendFile "Reservas.txt" reserva
+
+listarImoveis :: IO ()
+listarImoveis = do
+    putStrLn "Imóveis cadastrados no sistema: "
+    imoveisTxt <- readFile "Imoveis.txt"
+    let imoveis = lines imoveisTxt
+    mostraImovel imoveis
+
+mostraImovel :: [String] -> IO ()
+mostraImovel (h:t) = do
+    --let imovel = splitOn "," h
+    --putStrLn "Id do imóvel: " ++ show (imovel!!0)
+    putStrLn "Mostrar imóvel"
 
 main :: IO ()
 main = do
